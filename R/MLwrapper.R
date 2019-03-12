c7c0b76021d5edec964a9ec1a1c410d5b1ca5ad7
Sarah <sarah.reehl@pnnl.gov>
2019-03-12 09:42:40 -0700

    adding ML wrapper function

---
+++ R/MLwrapper.R
@@ -0,0 +1,230 @@
+#' Applies ML algorithms to partitioned data
+#' 
+#' This function applies ML algorithms to 'MLinput' object 
+#' 
+#' @param data_object object of class 'MLinput' a list of two data frames containing train and test rows to subset in x_data and y_data
+#' @param methods a vector of strings specifying which ML algorithm you want to implement? options: 'bglr', 'clogit'. The length of this vector must be equal to the number of data sources in data_object
+#' @param single_source is a character string identifying which data source (when there is more than 1 data source in data_object) to apply ML to. Defaults to NULL
+#'
+#' @details 
+#' 
+#' @examples 
+#' @export
+MLwrapper = function(data_object, methods, scale_and_center = FALSE, single_source = NULL) {
+    # match 'method' argument with available options
+    optns <- c("svm", "rf", "nb", "knn", "lda")
+    
+    if (!(all(methods %in% optns))) 
+        stop(paste("Provided 'method' argument is invalid, please select from", c(optns), "."))
+    
+    # check that partition_info attr is present in data_object
+    if (is.null(attr(data_object, "partition_info"))) 
+        stop("dataPartitioning has not been applied")
+    
+    # extract number of sources
+    n_sources = attr(data_object, "n_sources")
+    
+    # check if single_source is provided then data_object must have more than one data source
+    if (!is.null(single_source) & n_sources == 1) 
+        stop("'single_source' can only be provided when 'data_object' has only one data source")
+    
+    # case where n_sources is 1
+    if (n_sources == 1) {
+        # extract parts from data_object
+        x_data = data_object$X
+        y_data = data_object$Y
+        
+        part_info = attr(data_object, "partition_info")
+        
+        # extract cnames
+        sample_cname = attr(data_object, "cnames")$sample_cname
+        outcome_cname = attr(data_object, "cnames")$outcome_cname
+        pair_cname = attr(data_object, "cnames")$pair_cname
+        
+        # sample_cname column index
+        samp_ind = which(names(x_data) %in% sample_cname)
+        
+        ml_rez = lapply(methods, MLwrapper_helper, x_data, y_data, part_info, scale_and_center, outcome_cname, pair_cname, samp_ind)
+        names(ml_rez) = methods
+        
+        attr(data_object, "ML_results") = ml_rez
+        attr(data_object, "ML_method") = methods
+        
+    } else if (n_sources > 1) {
+        # check that the length of 'method' vector is equal to n_sources, note this does not apply when 'single_source' argument is
+        # provided
+        if (is.null(single_source)) {
+            if (length(methods) != n_sources) 
+                stop("the number of methods provided in 'method' argument is not equal to the number of sources in 'data_object'")
+        }
+        
+        # extract parts from data_object
+        x_data = data_object$X
+        y_data = data_object$Y
+        
+        part_info = attr(data_object, "partition_info")
+        
+        # extract cnames
+        sample_cname = attr(data_object, "cnames")$sample_cname
+        outcome_cname = attr(data_object, "cnames")$outcome_cname
+        pair_cname = attr(data_object, "cnames")$pair_cname
+        
+        # sample_cname column index
+        samp_ind = which(names(x_data[[1]]) %in% sample_cname)
+        ml_rez_list = mapply(FUN = MLwrapper_helper, method = methods, X = x_data, MoreArgs = list(y_data, part_info, scale_and_center, 
+            outcome_cname, pair_cname, samp_ind))
+        ml_rez_list = as.data.frame(ml_rez_list)
+        ml_rez_list = lapply(ml_rez_list, as.data.frame, stringsAsFactors = FALSE)
+        
+        names(ml_rez_list) = paste(names(x_data), names(ml_rez_list), sep = ".")
+        
+        attr(data_object, "ML_results") = ml_rez_list
+        attr(data_object, "ML_method") = methods
+        
+        # case where single_source is provided
+        if (!is.null(single_source)) {
+            
+            # extract parts from data_object
+            x_data = data_object$X
+            y_data = data_object$Y
+            part_info = attr(data_object, "partition_info")
+            
+            # check that single_source is one of the data sources in data_object
+            if (!(single_source %in% names(x_data))) 
+                stop(paste(single_source, " is not one of the data source names of 'data_object'", sep = ""))
+            
+            # select the correct data source
+            x_data = x_data[which(names(x_data) %in% single_source)]
+            names(x_data) = NULL
+            x_data = as.data.frame(x_data)
+            
+            # extract cnames
+            sample_cname = attr(data_object, "cnames")$sample_cname
+            outcome_cname = attr(data_object, "cnames")$outcome_cname
+            pair_cname = attr(data_object, "cnames")$pair_cname
+            
+            # sample_cname column index
+            samp_ind = which(names(x_data) %in% sample_cname)
+            
+            ml_rez = lapply(methods, MLwrapper_helper, x_data, y_data, part_info, scale_and_center, outcome_cname, pair_cname, samp_ind)
+            names(ml_rez) = methods
+            
+            # add attributes to data_object
+            attr(data_object, "ML_results") = ml_rez
+            attr(data_object, "ML_method") = methods
+            attr(data_object, "single_source") = single_source
+            
+        }
+    }
+    
+    return(data_object)
+}
+
+
+
+
+MLwrapper_helper = function(method, X, data, partition_info, scale_and_center, outcome_cname, pair_cname, samp_ind) {
+    # extract partition_info
+    train_partitions = partition_info$train
+    test_partitions = partition_info$test
+    
+    # switch statement
+    ml_method <- switch(method, svm = peppuR_svm, rf = peppuR_rf, nb = peppuR_nb, knn = peppuR_knn, lda = peppuR_lda)
+    
+    # if knn, the model is memoryless and can't be stored
+    if (tolower(method) == "knn") {
+        pred_label_knn = mapply(function(train, test) {
+            train_partition = train$Train
+            test_partition = test$Test
+            model <- "memoryless"
+            
+            # figure out the best way to scale and center the data
+            X_train <- scale(X[train_partition, -samp_ind], scale = TRUE, center = TRUE)
+            X_test <- (X[test_partition, -samp_ind] - attr(X_train, "scaled:center"))/attr(X_train, "scaled:scale")
+            start <- Sys.time()
+            pred_prob <- attr(ml_method(X_train, X_test, train_class = as.factor(data[train_partition, outcome_cname]), prob = TRUE), 
+                "prob")
+            train_time <- Sys.time() - start
+            
+            pred_label <- as.numeric(apply(pred_prob, 1, function(x) names(x)[which.max(x)]))
+            truth <- data[test_partition, outcome_cname, drop = FALSE]
+            res <- data.frame(PredictedProbs = pred_prob, PredictedLabel = pred_label, Truth = truth)
+            attributes(res)$train_time <- train_time
+            return(res)
+            
+        }, train_partitions, test_partitions)
+        
+        pred_label_knn = as.data.frame(pred_label_knn)
+        pred_label_knn = lapply(pred_label_knn, as.data.frame, stringsAsFactors = FALSE)
+        result = pred_label_knn
+        
+    } else {
+        pred_labels = mapply(function(train, test) {
+            train_partition = train$Train
+            test_partition = test$Test
+            
+            if (scale_and_center) {
+                X_train <- scale(X[train_partition, -samp_ind, drop = FALSE], scale = TRUE, center = TRUE)
+                X_test <- scale(X[test_partition, -samp_ind, drop = FALSE], center = attr(X_train, "scaled:center"), scale = attr(X_train, 
+                  "scaled:scale"))
+            }
+            start <- Sys.time()
+            model <- try(ml_method(X = X[train_partition, -samp_ind, drop = FALSE], data = data[train_partition, , drop = FALSE], 
+                outcome_cname = outcome_cname, pair_cname = pair_cname))
+            train_time <- Sys.time() - start
+            
+            if (class(model) == "try-error") {
+                res <- data.frame(PredictedProbs.0 = rep(1, length(test_partition)), PredictedProbs.1 = rep(1, length(test_partition)), 
+                  PredictedLabel = rep(NA, length(test_partition)), Truth = data[test_partition, outcome_cname, drop = FALSE])
+                return(res)
+            }
+            
+            # lda uses a different syntax to return probabilities
+            if (method == "lda") {
+                pred_prob <- predict(model, newdata = X[test_partition, -samp_ind, drop = FALSE])$posterior
+            } else if (method == "rf") {
+                pred_prob <- predict(model, data = X[test_partition, -samp_ind, drop = FALSE])$predictions
+                #pred_prob <- data.frame(1 - as.numeric(pred_prob), pred_prob)
+                colnames(pred_prob) <- c("0", "1")
+            } else if (method == "svm") {
+                pred_prob <- predict(model, newdata = X[test_partition, -samp_ind, drop = FALSE])
+                pred_prob <- 1/(1 + exp(-pred_prob))
+                pred_prob <- data.frame(1 - pred_prob, pred_prob)
+                colnames(pred_prob) <- c("0", "1")
+            } else {
+                pred_prob <- predict(model, newdata = X[test_partition, -samp_ind, drop = FALSE])
+            }
+            
+            pred_label <- as.numeric(apply(pred_prob, 1, function(x) names(x)[which.max(x)]))
+            truth <- data[test_partition, outcome_cname, drop = FALSE]
+            res <- data.frame(PredictedProbs = pred_prob, PredictedLabel = pred_label, Truth = truth)
+            attributes(res)$train_time <- train_time
+            return(res)
+            
+        }, train_partitions, test_partitions)
+        
+        
+        
+        pred_labels = as.data.frame(pred_labels)
+        pred_labels = lapply(pred_labels, as.data.frame, stringsAsFactors = FALSE)
+        
+        # organize data
+        part_names = names(pred_labels)
+        
+        result = mapply(function(item, name) {
+            rez = cbind(item, rep(name, nrow(item)))
+            names(rez)[4] = "Truth"
+            names(rez)[5] = "Partition_info"
+            
+            return(rez)
+            
+        }, pred_labels, part_names)
+        
+        result = as.data.frame(result)
+        result = lapply(result, as.data.frame, stringsAsFactors = FALSE)
+        result = do.call(rbind, result)
+    }
+    
+    return(result)
+}
+
---
+++ R/dataPartitioning.R
@@ -0,0 +1,263 @@
+#' Returns information on how to partition y_data
+#' 
+#' This function returns information on how to partition (randomly or paired) y_data
+#' importFrom magrittr "%>%"
+#' @param data_object argument is the output produced by as.MLinput, which contains a single x data frame or a list of x data frames, a y data frames and attributes
+#' @param partition_style one of 'random' or 'paired' (character string) indicating type of partition
+#' @param folds integer of k for k-fold cross validation
+#' @param repeats integer of number of iterations to repeat cross validation
+#' @param holdout_perc numeric between 0 and 1 indicating the percentage of data to withold for the holdout validation set
+#' 
+#' @details 
+#' 
+#' @examples 
+#' @export
+
+dataPartitioning = function(data_object, partition_style = "random", folds = 4, repeats = 100, holdout_perc = 0.25) {
+    # extract y_data from data_object and cnames
+    y_data = data_object$Y
+    
+    group_identifier = attr(data_object, "cnames")$outcome_cname
+    pair_identifier = attr(data_object, "cnames")$pair_cname
+    
+    # initial checks
+    if (!(partition_style %in% c("random", "paired"))) 
+        stop("'partition_style' must be one of, 'random' or 'paired'")
+    if (!inherits(y_data, "data.frame")) 
+        stop("'y_data' must be of class 'data.frame'")
+    
+    if (partition_style == "random" & !is.null(pair_identifier)) 
+        stop("'pair_identifier' is only used with 'paired' partition_style")
+    
+    repeats_new = repeats * 1.5
+    repeats_extra = repeats_new - repeats
+    
+    if (partition_style == "random") {
+        partition_result = pureRandomTestingTraining(data = y_data, group_identifier = group_identifier, folds = folds, repeats = repeats, 
+            holdout_perc = holdout_perc)
+        
+        # rearrrange partition_result data into test data and training data
+        train_list = lapply(partition_result, function(item) {
+            item$train
+        })
+        test_list = lapply(partition_result, function(item) {
+            item$test
+        })
+        
+        # form fold and repeat lists
+        fld = rep(1:folds, repeats)
+        fld = as.list(fld)
+        
+        reps = as.list(1:repeats)
+        reps = lapply(reps, rep, folds)
+        reps = unlist(reps)
+        reps = as.list(reps)
+        
+        # form folds repeats data frames
+        train_df = mapply(fold_rep_train, train_list, fld, reps)
+        train_df = as.data.frame(train_df)
+        train_df = lapply(train_df, as.data.frame, stringsAsFactors = F)
+        train_df = do.call(rbind, train_df)
+        
+        test_df = mapply(fold_rep_test, test_list, fld, reps)
+        test_df = as.data.frame(test_df)
+        test_df = lapply(test_df, as.data.frame, stringsAsFactors = F)
+        test_df = do.call(rbind, test_df)
+        
+        final_result = list(train = train_df, test = test_df)
+        final_result = lapply(final_result, function(x) {
+            split(data.table::as.data.table(x), by = c("Fold", "Rep"))
+        })
+        
+        ########## partition data for repeats_extra########## ##########
+        partition_result_extra = pureRandomTestingTraining(data = y_data, group_identifier = group_identifier, folds = folds, repeats = repeats_extra, 
+            holdout_perc = holdout_perc)
+        
+        # rearrrange partition_result data into test data and training data
+        train_list = lapply(partition_result_extra, function(item) {
+            item$train
+        })
+        test_list = lapply(partition_result_extra, function(item) {
+            item$test
+        })
+        
+        # form fold and repeat lists
+        fld = rep(1:folds, repeats_extra)
+        fld = as.list(fld)
+        
+        reps = as.list(1:repeats_extra)
+        reps = lapply(reps, rep, folds)
+        reps = unlist(reps)
+        reps = as.list(reps)
+        
+        # form folds repeats data frames
+        train_df = mapply(fold_rep_train, train_list, fld, reps)
+        train_df = as.data.frame(train_df)
+        train_df = lapply(train_df, as.data.frame, stringsAsFactors = F)
+        train_df = do.call(rbind, train_df)
+        
+        test_df = mapply(fold_rep_test, test_list, fld, reps)
+        test_df = as.data.frame(test_df)
+        test_df = lapply(test_df, as.data.frame, stringsAsFactors = F)
+        test_df = do.call(rbind, test_df)
+        
+        final_result_extra = list(train = train_df, test = test_df)
+        final_result_extra = lapply(final_result_extra, function(x) {
+            split(data.table::as.data.table(x), by = c("Fold", "Rep"))
+        })
+        
+    } else if (partition_style == "paired") {
+        partition_result = pairedCaseControlTestingTraining(data = y_data, group_identifier = group_identifier, pair_identifier = pair_identifier, 
+            folds = folds, repeats = repeats)
+        
+        # rearrrange partition_result data into two tibble data frames, one for test data one for train data
+        train_list = lapply(partition_result, function(item) {
+            item$train
+        })
+        test_list = lapply(partition_result, function(item) {
+            item$test
+        })
+        
+        # form fold and repeat lists
+        fld = rep(1:folds, repeats)
+        fld = as.list(fld)
+        
+        reps = as.list(1:repeats)
+        reps = lapply(reps, rep, folds)
+        reps = unlist(reps)
+        reps = as.list(reps)
+        
+        # form folds repeats data frames
+        train_df = mapply(fold_rep_train, train_list, fld, reps)
+        train_df = as.data.frame(train_df)
+        train_df = lapply(train_df, as.data.frame, stringsAsFactors = F)
+        train_df = do.call(rbind, train_df)
+        
+        test_df = mapply(fold_rep_test, test_list, fld, reps)
+        test_df = as.data.frame(test_df)
+        test_df = lapply(test_df, as.data.frame, stringsAsFactors = F)
+        test_df = do.call(rbind, test_df)
+        
+        final_result = list(train = train_df, test = test_df)
+        final_result = lapply(final_result, function(x) {
+            split(data.table::as.data.table(x), by = c("Fold", "Rep"))
+        })
+        
+        ########## partition data for repeats_extra########## ##########
+        partition_result_extra = pairedCaseControlTestingTraining(data = y_data, group_identifier = group_identifier, pair_identifier = pair_identifier, 
+            folds = folds, repeats = repeats)
+        
+        # rearrrange partition_result data into two tibble data frames, one for test data one for train data
+        train_list = lapply(partition_result_extra, function(item) {
+            item$train
+        })
+        test_list = lapply(partition_result_extra, function(item) {
+            item$test
+        })
+        
+        # form fold and repeat lists
+        fld = rep(1:folds, repeats_extra)
+        fld = as.list(fld)
+        
+        reps = as.list(1:repeats_extra)
+        reps = lapply(reps, rep, folds)
+        reps = unlist(reps)
+        reps = as.list(reps)
+        
+        # form folds repeats data frames
+        train_df = mapply(fold_rep_train, train_list, fld, reps)
+        train_df = as.data.frame(train_df)
+        train_df = lapply(train_df, as.data.frame, stringsAsFactors = F)
+        train_df = do.call(rbind, train_df)
+        
+        test_df = mapply(fold_rep_test, test_list, fld, reps)
+        test_df = as.data.frame(test_df)
+        test_df = lapply(test_df, as.data.frame, stringsAsFactors = F)
+        test_df = do.call(rbind, test_df)
+        
+        final_result_extra = list(train = train_df, test = test_df)
+        final_result_extra = lapply(final_result_extra, function(x) {
+            split(data.table::as.data.table(x), by = c("Fold", "Rep"))
+        })
+    }
+    
+    attr(data_object, "partition_info") = final_result
+    attr(data_object, "extra_partitions") = final_result_extra
+    attr(data_object, "foldrep") = list(folds = folds, repeats = repeats)
+    
+    return(data_object)
+}
+
+
+pureRandomTestingTraining <- function(data, group_identifier, folds = 4, repeats = 100, holdout_perc = 0.25) {
+    
+    groups <- data[, group_identifier]
+    seed <- 42
+    
+    #------ determine training indeces ----------#
+    # returns a k x repeats size list of training indeces set seed for reproducability purposes
+    set.seed(42)
+    training_ind_list <- caret::createMultiFolds(y = groups, k = folds, times = repeats)
+    
+    #------ diff data and train to return testing indeces -----#
+    # returns a k x repeats size list of grouped testing and training indeces
+    ind_list <- lapply(training_ind_list, function(training_inds) {
+        partition_list <- list()
+        partition_list$train <- training_inds
+        partition_list$test <- setdiff(1:nrow(data), training_inds)
+        return(partition_list)
+    })
+    return(ind_list)
+}
+
+
+
+pairedCaseControlTestingTraining <- function(data, group_identifier, pair_identifier, folds = 4, repeats = 100, holdout_set = NULL) {
+    
+    tuning_set <- FALSE
+    seed <- 42
+    
+    #------- create a map to keep orignal indexing in tact -----#
+    data$old_index <- 1:nrow(data)
+    #------- remove the holdout set if there is one ---------#
+    if (length(holdout_set) > 0) {
+        data <- data[-which(data[, pair_identifier] %in% holdout_set), ]
+    }
+    #------- use map to find unique pairs -----#
+    singled_pair_subset <- data %>% dplyr::arrange_(group_identifier) %>% dplyr::distinct_(pair_identifier, .keep_all = TRUE)  #keep only unique identifiers for training subset
+    singled_pair_subset[1:nrow(singled_pair_subset)/2, group_identifier] <- 1  # convert half the labes to zeros for caret's balancing act  
+    index_map <- data.frame(old = singled_pair_subset$old_index, new = 1:nrow(singled_pair_subset), pair = singled_pair_subset[, pair_identifier])
+    rownames(index_map) <- index_map$new
+    
+    #------ vector of group labels to split ------#
+    group <- singled_pair_subset[, group_identifier]
+    
+    #------ determine training indeces ----------#
+    # returns a k x repeats size list of training indeces
+    training_ind_list <- caret::createMultiFolds(y = group, k = folds, times = repeats)
+    
+    #------- match case-control pairs --------#
+    ind_list <- lapply(training_ind_list, function(x) {
+        pairs <- index_map[x, "pair"]
+        partition_list <- list()
+        partition_list$train <- data[which(data[, pair_identifier] %in% pairs), "old_index"]
+        partition_list$test <- data[which(!data[, pair_identifier] %in% pairs), "old_index"]
+        return(partition_list)
+    })
+    #------ diff data and train to return testing indeces -----#
+    # returns a k x repeats size list of grouped testing and training indeces ind_list <- lapply(training_ind_list_mapped,
+    # function(training_inds){ pairs <- index_map[x, 'pair'] partition_list <- list() partition_list$train <- training_inds
+    # partition_list$test <- data[which(!data[,pair_identifier] %in% pairs), 'old_index'] return(partition_list) })
+    
+}
+
+# folds repeats helper functions for training data and test data
+fold_rep_train = function(train_vec, fold, repp) {
+    result = data.frame(Fold = rep(fold, length(train_vec)), Rep = rep(repp, length(train_vec)), Train = train_vec)
+    return(result)
+}
+
+fold_rep_test = function(train_vec, fold, repp) {
+    result = data.frame(Fold = rep(fold, length(train_vec)), Rep = rep(repp, length(train_vec)), Test = train_vec)
+    return(result)
+}
