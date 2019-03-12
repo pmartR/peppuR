ddf045d94deb09632aa1caf0b62b6b6878551671
Sarah <sarah.reehl@pnnl.gov>
2019-03-12 09:44:38 -0700

    naive bayes

---
+++ R/naive_bayes.R
@@ -0,0 +1,26 @@
+# require(e1071)
+require(naivebayes)
+
+#' @param X - n-by-p data.frame containing all of the 'covariates' that will be used to predict class of y
+#' @param data - a data.frame with columns:
+#'           y - n vector of class identifiers for each for of X
+#'          ID - n vector of patient IDs
+#'        pair - vector of pair identifiers
+#'        time - n vector of time point identifiers
+#' @param ... - additional arguements passed to BGLR(...)
+#' 
+#' @return the object created by BGLR
+
+peppuR_nb <- function(X, data, pair_cname, ...) {
+    
+    if (length(data[[pair_cname]]) > 0) {
+        # If pair information was provided, add it to the data matrix warning('There is probably a better way to include pair information
+        # into an SVM')
+        X <- cbind(X, pair = as.factor(data[[pair_cname]]))
+    }
+    # set.seed(42) nb_fit <- naiveBayes(x=X, y=data$y,...)
+    nb_fit <- naive_bayes(x = X, y = as.factor(data[[pair_cname]]))
+    
+    return(nb_fit)
+}
+
